d99b0bff0784f23ee519890fd54a22609fc7cee6
JuliFiumana <124445670+JuliFiumana@users.noreply.github.com>
2023-02-04 10:59:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let entrada = prompt ('Bienvenido al detector TACC. Ingrese su ingrediente');
+
+switch (entrada) {
+    case 'trigo':
+    {alert ("No es apto celíacos")} 
+break
+    case 'cebada':
+    {alert ("No es apto celíacos")} 
+break
+    case'centeno':
+    {alert ("No es apto celíacos")} 
+break
+    case 'avena':
+    {alert ("No es apto celíacos")} 
+break
+    default: {alert ("Es apto celíacos")}
+break };
+
+while (entrada != ("trigo" || "cebada" || "centeno" ||"avena" ))
+{alert ("Tenemos recetas apto celíacos para usted")};
+
+
+
+
+
+
